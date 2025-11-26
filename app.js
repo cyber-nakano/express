c52bf7d872166be40df069e5909f8d2d936eea22
cyber-nakano <2401355024qu@cyber-u.ac.jp>
2025-11-26 16:10:29 +0900

    Add zip api-2nd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require("./routes/users");
 var helloRouter = require("./routes/hello");
 var notesRouter = require("./routes/notes");
 var catRouter = require("./routes/cat");
-
+var zipRouter = require("./routes/zip");
 var app = express();
 
 // view engine setup
@@ -27,6 +27,8 @@ app.use("/users", usersRouter);
 app.use("/hello", helloRouter);
 app.use("/notes", notesRouter);
 app.use("/cat", catRouter);
+app.use("/zip", zipRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
