572387eb08b1723cc509308784213a7ea3a2657e
cyber-nakano <2401355024qu@cyber-u.ac.jp>
2025-11-26 16:15:09 +0900

    Add zip api-3rd

--- routes/zip.js
+++ routes/zip.js
@@ -3,10 +3,10 @@ var router = express.Router();
 const request = require("request");
 
 router.get("/", async (req, res) => {
-  //   let zipcode = req.query["zipcode"];
-  //   console.log(zipcode);
+  let zipcode = req.query["zipcode"];
+  console.log(zipcode);
   request(
-    "https://zipcloud.ibsnet.co.jp/api/search?zipcode=4610001", //+ zipcode,
+    "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" + zipcode,
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         const data = JSON.parse(body);
